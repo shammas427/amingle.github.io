4b1ebef1ac409296a40d67a511874c42c63c9427
miuxo <shammas7809@gmail.com>
2026-06-12 13:59:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,94 +8,48 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server,{
-cors:{
-origin:"*"
+const io = new Server(server, {
+cors: {
+origin: "*"
 }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({
-extended:true
-}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-
+app.get("/", (req, res) => {
 res.sendFile(
-path.join(
-__dirname,
-"public",
-"index.html"
-)
+path.join(__dirname, "public", "index.html")
 );
-
 });
 
 let onlineUsers = 0;
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
 onlineUsers++;
+io.emit("online-users", onlineUsers);
 
-io.emit(
-"online-users",
-onlineUsers
-);
+console.log("User Connected");
 
-console.log(
-"User Connected"
-);
+socket.on("chat-message", (message) => {
+io.emit("chat-message", {
+text: message,
+time: new Date().toLocaleTimeString()
+});
+});
 
-socket.on(
-"chat-message",
-(message)=>{
-
-```
-  io.emit(
-    "chat-message",
-    {
-      text:message,
-      time:new Date()
-      .toLocaleTimeString()
-    }
-  );
-
-}
-```
-
-);
-
-socket.on(
-"disconnect",
-()=>{
-
-```
-  onlineUsers--;
-
-  io.emit(
-    "online-users",
-    onlineUsers
-  );
-
-  console.log(
-    "User Disconnected"
-  );
-
-}
-```
-
-);
+socket.on("disconnect", () => {
+onlineUsers--;
+io.emit("online-users", onlineUsers);
+console.log("User Disconnected");
+});
 
 });
 
-server.listen(PORT,()=>{
-
-console.log(
-`🚀 Server Running On Port ${PORT}`
-);
-
+server.listen(PORT, () => {
+console.log(`🚀 Server Running On Port ${PORT}`);
 });
