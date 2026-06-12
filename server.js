6a874d8c91591583919a64133b659b9eb2d10d2b
miuxo <shammas7809@gmail.com>
2026-06-12 07:58:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,13 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const path = require("path");
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
-let waiting = null;
-
-io.on("connection", (socket) => {
-  if (waiting) {
-    socket.partner = waiting;
-    waiting.partner = socket;
-
-    socket.emit("matched");
-    waiting.emit("matched");
-
-    waiting = null;
-  } else {
-    waiting = socket;
-  }
-
-  socket.on("message", (msg) => {
-    if (socket.partner) {
-      socket.partner.emit("message", msg);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    if (waiting === socket) waiting = null;
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
