e3d3d7d2b6f0bd9d09a2bf3068899d682200f0e7
miuxo <shammas7809@gmail.com>
2026-06-12 11:10:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,99 +1,37 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
 
-app.use(express.json());
-app.use(express.static(__dirname));
-
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-const UserSchema = new mongoose.Schema({
-  username:String,
-  email:String,
-  password:String,
-  role:{
-    type:String,
-    default:"user"
+const io = new Server(server,{
+  cors:{
+    origin:"*"
   }
 });
 
-const User = mongoose.model("User", UserSchema);
+io.on("connection",(socket)=>{
 
-app.post("/register", async(req,res)=>{
+  console.log("User Connected");
 
-  const {username,email,password} = req.body;
+  socket.on("chat-message",(msg)=>{
 
-  const hash = await bcrypt.hash(password,10);
+    socket.broadcast.emit(
+      "chat-message",
+      msg
+    );
 
-  const user = new User({
-    username,
-    email,
-    password:hash
   });
 
-  await user.save();
+  socket.on("disconnect",()=>{
 
-  res.json({
-    message:"Registration Successful"
-  });
+    console.log("User Left");
 
-});
-
-app.post("/login", async(req,res)=>{
-
-  const {email,password} = req.body;
-
-  const user = await User.findOne({email});
-
-  if(!user){
-    return res.json({
-      message:"User Not Found"
-    });
-  }
-
-  const match =
-  await bcrypt.compare(
-    password,
-    user.password
-  );
-
-  if(!match){
-    return res.json({
-      message:"Wrong Password"
-    });
-  }
-
-  const token = jwt.sign({
-    id:user._id,
-    role:user.role
-  },
-  process.env.JWT_SECRET);
-
-  res.json({
-    token,
-    username:user.username
   });
 
 });
 
-app.post("/forgot-password",(req,res)=>{
-
-  res.json({
-    message:"Password reset feature ready"
-  });
-
-});
-
-app.listen(
-process.env.PORT,
-()=>{
-console.log(
-"Server Running"
-);
+server.listen(3000,()=>{
+  console.log("Running");
 });
