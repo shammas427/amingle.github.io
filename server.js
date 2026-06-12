4800f9ec0118b64cce8cdc52948b4297b4267b03
miuxo <shammas7809@gmail.com>
2026-06-12 12:23:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req,res)=>{
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(3000, () => {
-  console.log("Server Running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+  console.log("Server Running on Port", PORT);
 });
